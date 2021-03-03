9c0c255bbaa1432a4929519ef122a5809edf934c
Gabriel Aguena <gaguena@gmail.com>
2021-03-03 17:19:43 -0300

    Criado paginas de detalhe

--- src/controller/task-controller.js
+++ src/controller/task-controller.js
@@ -5,13 +5,17 @@ const taskRouter = express.Router();
 
 const TaskController  = {
 
-    task: (req, resp) => {
+    taskOne:(req, resp) => {
+        console.log('Recebendo o parametro: ' + req.params.code);
+        resp.redirect('/tasks')
+    },
+    tasksAll: (req, resp) => {
         new TaskService().findAll().then((datas) => {
             console.log("Resultado:" + datas);
-            resp.render('task/task-page', {tasks: datas});
+            resp.render('task/tasks-page', {tasks: datas});
         }).catch((error) => {
             console.log('Erro: ' + error);
-            resp.render('task/task-page');
+            resp.render('task/tasks-page', {tasks: {}});
         });
     },
     save: (req, resp) => {
@@ -22,7 +26,8 @@ const TaskController  = {
 }
 
 
-taskRouter.get("/", TaskController.task);
+taskRouter.get("/", TaskController.tasksAll);
+taskRouter.get("/:code/", TaskController.taskOne);
 taskRouter.post("/", TaskController.save);
 
 module.exports = taskRouter;
